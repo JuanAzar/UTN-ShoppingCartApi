8eac86d78d4fd41c4e6dcc53f1e7bf757aa6bc17
Juan Azar <juan.azar@gmail.com>
2024-11-10 23:13:57 -0300

    New Shopping Cart Api

---
+++ ShoppingCart.Api/ViewModels/LoginCredentialsVM.cs
@@ -0,0 +1,8 @@
+namespace ShoppingCart.Api.ViewModels
+{
+    public class LoginCredentialsVM
+    {
+        public string Emails { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
